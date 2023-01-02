2ce7f0f5ad48ed99fe8ebb56198c2649a5a778e5
lukemulholland <106896894+lukemulholland@users.noreply.github.com>
2023-01-02 17:37:56 +1100

    Add label and images

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Udder")
+                .font(.title)
+            Text("Fresh Milk, On Demand")
         }
         .padding()
     }
