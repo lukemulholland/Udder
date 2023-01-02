120aa98f4b8a04b5a490f63d8607fd28ce2083be
lukemulholland <106896894+lukemulholland@users.noreply.github.com>
2023-01-02 17:35:30 +1100

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Luke Mulholland on 2/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
